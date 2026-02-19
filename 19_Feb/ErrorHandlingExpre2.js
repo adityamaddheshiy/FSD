2bd8f0517fae5f0a166e6a5fd40588d31f42d343
Aditya <adityamaddheshiya669@gmail.com>
2026-02-19 15:18:11 +0530

    .\ErrorHandlingExpre2.js

---
+++ 19_Feb/ErrorHandlingExpre2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+fs.readFile(`foo.txt`,function (err,data){
+    if(err){
+        console.log();
+    }
+    else{
+        console.log(data.toString());
+    }
+    
+});
+console.log("program Ended");
